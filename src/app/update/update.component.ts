9cfe9e68e283f62960c7ec62d4e11230f018fd06
luis <langel7630@gmail.com>
2023-05-17 09:33:49 +0200

    ..

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../servicios/api.service';
 import { User } from '../formulario/models/user.interface';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Group } from '../formulario/models/group.interface';
 
 @Component({
   selector: 'app-update',
@@ -9,23 +10,38 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
 })
 export class UpdateComponent implements OnInit {
   constructor(
-    private apiService: ApiService,
+    private ApiService: ApiService,
     private router: Router,
     private routeActivated: ActivatedRoute
   ) {}
 
+
   user!: User;
   errorMsg!: string;
+  groups: Group[] = [];
 
   ngOnInit(): void {
     const userId = this.routeActivated.snapshot.params['userId'];
-    this.apiService.getUser(userId).subscribe((data: User) => {
+    this.ApiService.getUser(userId).subscribe((data: User) => {
       this.user = data; // Asigna los datos del usuario obtenidos al objeto user
+    });
+
+    this.loadGroups();
+
+  }
+loadGroups() {
+    this.ApiService.getGroups().subscribe((groups) => {
+      this.groups = groups;
+    });
+  }
+  assignGroupToUser(user: any) {
+    this.ApiService.addUserToGroup(user.groupId, user.id).subscribe(() => {
+      console.log('Usuario asignado al grupo correctamente');
     });
   }
 
   procesarUpdate() {
-    this.apiService.updateUser(this.user).subscribe({
+    this.ApiService.updateUser(this.user).subscribe({
       next: () => {
         this.router.navigate(['userstbl']);
       },
